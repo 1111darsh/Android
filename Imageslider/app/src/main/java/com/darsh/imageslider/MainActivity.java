5ba41727fd1396e04f4e5299f087a445584c65b6
Darsh Mistry <1111darsh@gmail.com>
2019-04-05 23:25:14 +0530

    m
    m

--- Imageslider/app/src/main/java/com/darsh/imageslider/MainActivity.java
+++ Imageslider/app/src/main/java/com/darsh/imageslider/MainActivity.java
@@ -3,15 +3,26 @@ package com.darsh.imageslider;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    int x;
+    ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ViewPager viewPager = findViewById(R.id.viewpager);
+        viewPager = findViewById(R.id.viewpager);
         ImageAdpater adapter = new ImageAdpater(this);
         viewPager.setAdapter(adapter);
-    }
+        x =viewPager.getCurrentItem();
+        Button next = findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem( x +1);
+            }
+        });
+        }
 }
